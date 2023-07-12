71b818f9f3c549f9ccf0f073ac1b4294bce993b9
CAFUCH <tjfdk6201@gmail.com>
2023-07-12 17:58:33 +0900

    230712
    A+B - 5

--- CordingTest_note/CordingTest_note/오픈채팅방.cpp
+++ CordingTest_note/CordingTest_note/오픈채팅방.cpp
@@ -7,19 +7,20 @@
 //using namespace std;
 //
 //vector<string> solution(vector<string> record) {
-//    //https://school.programmers.co.kr/learn/courses/30/lessons/42888
 //
 //    vector<string> answer;
 //    map<string, string> member; // id와 name 저장용도
-//    string state, id, name, key; // 통
+//    string state, id, name; // 통
 //
-//    for (int i = 0; i < record.size(); ++i) { // 안 돌아
+//    for (int i = 0; i < record.size(); i++) { // 안 돌아
+//
+//        cout << "들어옴";
 //
 //        stringstream stream(i);
 //        stream >> state; stream >> id; stream >> name; // 한 세트를 받으면?
 //
-//        switch (state.at(0))
-//        {
+//        switch (state.at(0)) {
+//
 //            case 'E' :
 //                member[id] = name;
 //                answer.push_back(id + "님이 들어왔습니다.");
@@ -35,19 +36,20 @@
 //        }
 //    }
 //
-//    for (int i = 0; i < record.size(); ++i) {
+//    //for (auto i : answer) {
 //
-//        key = answer[i].substr(0, answer[i].find("님"));
-//        answer[i].replace(0, answer[i].find("님"), member[key]);
-//    }
+//    //    key = i.substr(0, i.find("님"));
+//    //    i.replace(0, i.find("님"), member[key]);
+//    //}
 //
-//    for (int i = 0; i < answer.size(); ++i)
-//        cout << answer[i] << endl;
 //    return answer;
 //}
 //
 //int main() {
 //    
-//    vector<string> d = { "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan" };
-//    solution(d);
+//    vector<string> d { "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan" };
+//    vector<string> ans = solution(d);
+//
+//    for (auto a : ans)
+//        cout << a;
 //}
