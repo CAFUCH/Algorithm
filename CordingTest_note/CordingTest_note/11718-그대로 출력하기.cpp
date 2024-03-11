1995ae398a4ca678feca7dd3da6c9e451088047d
CAFUCH <tjfdk6201@gmail.com>
2024-03-11 23:08:52 +0900

    24-3-2

---
+++ CordingTest_note/CordingTest_note/11718-그대로 출력하기.cpp
@@ -0,0 +1,10 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{//https://www.acmicpc.net/problem/11718
+//	string str;
+//	while (getline(cin, str))
+//		cout << str << '\n';
+//}
---
+++ CordingTest_note/CordingTest_note/1463-1로 만들기.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//int dp[1000001] = { 0 };
+//
+//int main()
+////https://www.acmicpc.net/problem/1463
+//{
+//	int n;
+//
+//	cin >> n;
+//
+//	for (int i = 2; i <= n; ++i)
+//	{
+//		dp[i] = 1 + dp[i - 1];
+//		if (i % 3 == 0)
+//			dp[i] = min(1 + dp[i / 3], dp[i]);
+//		if (i % 2 == 0)
+//			dp[i] = min(1 + dp[i / 2], dp[i]);
+//	}
+//
+//	cout << dp[n];
+//}
